1a221937d9fb6fadd28fca7583241500298ed485
cortezmb <michael.cortez78@gmail.com>
2020-06-29 16:02:29 -0500

    buildDeck, Shuffle, dealerCards, playerCards, displayDealerCards, displayPlayerCards aare now working

--- blackjack.js
+++ blackjack.js
@@ -1,58 +1,133 @@
-//in your JS file, add the following code.  This code "listens" for the user to click the button, and then executes the //code inside the code block.  Note that the listenter takes as arguments, the event, and and annonymous function.  
 
-function dealDeck(){
+//Global variables
+var suits = ['H', 'D', 'C', 'S'];
+var values = [2,3,4,5,6,7,8,9,10,'J', 'Q', 'K', 'A'];
+var deck = [];
+var playerHand = [];
+var dealerHand = [];
+var dealButton = document.getElementById("deal-button");
+var player_Deck = document.getElementById("player-deck");
+var dealer_Deck = document.getElementById("dealer-deck");
 
-    var dealButton = document.getElementById("#deal-button");
-    var player_Deck = document.getElementById("#player-deck");
-    var dealer_Deck = document.getElementsById("#dealer-deck");
+//Build the deck of cards
+function buildDeck(){
+    for(var a = 0; a < values.length; a++){
+        
+        for(var b = 0; b < suits.length; b++){
 
-    dealButton.addEventListener("click", (event) => {
-        console.log("hello");
-        dealer_Deck.innerHTML = <img src="images/JPEG/2C.jpg" alt=""></img>;
-        dealer_Deck.innerHTML = <img src="images/JPEG/2S.jpg" alt=""></img>;
-        player_Deck.innerHTML = <img src="images/JPEG/JS.jpg" alt=""></img>;
-        player_Deck.innerHTML = <img src="images/JPEG/KH.jpg" alt=""></img>;
-    else
-        dealer_Deck.innerHTML = '';
-        player_Deck.innerHTML = '';
-        // console.log("bye");
-    })
+            var card = {Value: values[a], Suit: suits[b], ImgFile: "images/JPEG/" + values[a] + suits[b] + ".jpg"};
+            
+            if(values[a] == 'J' || values[a] == 'Q' || values[a] == 'K'){
+                card.Value = 10;
+            }
+            if(values[a] == 'A'){
+                card.Value = 11;
+            }
+
+            deck.push(card);
+        }
+    }
 }
 
 
 
 
-// document.getElementById("btnHit").addEventListener("click", function(){
-//     //write "hit" logic here
-// });
+//Shuffle the deck of cards
+function shuffle(deck) {
+    for (var i = deck.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp =deck[i];
+       deck[i] =deck[j];
+       deck[j] = temp;
+    }
+    return deck;
+}
+
+
+
+
+//Get 2 cards from deck for Dealer
+function dealerCards(){
+    dealerHand.push(deck.pop());
+    dealerHand.push(deck.pop());
+    var imageCard = "";
+    for(var i = 0; i < dealerHand.length; i++){
+        imageCard += `<img src="${dealerHand[i].ImgFile}" width = "200px"></img>`;
+    }
+    return imageCard
+}
+
+//Get 2 cards from deck for Player
+function playerCards(){
+    playerHand.push(deck.pop());
+    playerHand.push(deck.pop());
+    var imageCard = "";
+    for(var i = 0; i < playerHand.length; i++){
+        imageCard += `<img src="${playerHand[i].ImgFile}" width = "200px"></img>`;
+    }
+    return imageCard;
+}
+
+//Put 2 cards for Dealer in Dealer's Box
+function displayDealerCards(){
+    dealer_Deck.innerHTML = dealerCards();
+}
+
+//Put 2 cards for Player in Player's Box
+function displayPlayerCards(){
+    player_Deck.innerHTML = playerCards();
+}
+
+//Deal the deck
+
+dealButton.addEventListener("click", (event) => {
+    buildDeck();
+    //step 2 
+
+    deck = shuffle(deck)
+    // shuffle();
+    // dealerCards();
+    displayDealerCards();
+    // playerCards();
+    displayPlayerCards();
+})      
+
+
+
+
+// function hitMe(){
+
+//     hitButton = addEventListener("click", (event) => {
+//         player_Deck.innerHTML = buildDeck();
+//     })
+// }
+
+
 
 // document.getElementById("btnStand").addEventListener("click", function(){
 //     //write "Stand" logic here
 // });
 
-// /**
-//  * Randomize array element order in-place.
-//  * Using Durstenfeld shuffle algorithm.
-//  */
-
-// // function shuffleArray(array) {
-// //     for (var i = array.length - 1; i > 0; i--) {
-// //         var j = Math.floor(Math.random() * (i + 1));
-// //         var temp = array[i];
-// //         array[i] = array[j];
-// //         array[j] = temp;
-// //     }
-// //     return array;
-// // }
-
-// //Deck of cards
-// var suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
-// var values = [2,3,4,5,6,7,8,9,10,'Jack', 'Queen', 'King', 'Ace'];
-// var deck = [];
 
 
 
-// function buildDeck(){
 
+// var deck = [{suit: value, value: value, imgUrl: value}, {}, {}] //52 objects
+// // shuffle
+// var player = [{suit, value, imgURL}, {}, {}]; //19
 
-// }
+// var dealer = [{}, {}, {}, {}]; // 10, 17, 20
+
+//show them on the html using dom manipulion
+
+// calculation player hand ; add up the cards in the array
+
+// stand, hit
+
+// calculation
+
+// stand
+
+// dealer hand value
+
+// dealer get another card
